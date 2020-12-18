0b1fd57f18be35778cc6622a6b61954ff51a362d
Kaija Gahm <kaija.gahm@gmail.com>
2020-12-18 12:08:29 -0500

    Separated out the wrapper function into its own file

--- R/preprocessingFuns.R
+++ R/preprocessingFuns.R
@@ -77,24 +77,10 @@ fixNames <- function(df){
 #' @param df Raw survey data as a data frame.
 #' @export
 
-worcesterRemove <- function(df){
+removeGeo <- function(df){
   df <- df %>%
     select(-contains("ZIP")) %>%
     select(-contains("City-US"))
   return(df)
-}
-
-#' Preprocess data
-#'
-#' Wrapper function that combines `fixNames` and `worcesterRemove` into a single function
-#' @param df Raw survey data as a data frame. Must have column 'responseID'.
-#' @param updateID Character string that will become the `updateID` column for this table. For example, "survey11Add" for Survey 11.
-#' @export
-#'
-
-preprocessData <- function(df, updateID){
-  df1 <- fixNames(df)
-  df2 <- worcesterRemove(df1)
-  return(df2)
 }
 
---
+++ R/preprocessingWrapper.R
@@ -0,0 +1,12 @@
+#' Preprocess data
+#'
+#' Wrapper function that combines `fixNames` and `worcesterRemove` into a single function
+#' @param df Raw survey data as a data frame. Must have column 'responseID'.
+#' @param updateID Character string that will become the `updateID` column for this table. For example, "survey11Add" for Survey 11.
+#' @export
+
+preprocessData <- function(df, updateID){
+  df1 <- fixNames(df)
+  df2 <- worcesterRemove(df1)
+  return(df2)
+}
