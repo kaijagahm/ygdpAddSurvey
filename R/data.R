93d352cfc9fd7374900d5acc586b83a3860bd85c
Kaija Gahm <kaija.gahm@gmail.com>
2020-12-21 14:59:59 -0500

    fixed files and documentation for the dialect region polygons

---
+++ R/data.R
@@ -0,0 +1,83 @@
+#' US census data by county
+#'
+#' County-level demographic data from the US census (2010)
+#'
+#' @docType data
+#'
+#' @usage data(counties)
+#'
+#' @format An object of class \code{"sf", "data.frame"}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(counties)
+"counties"
+
+#' US census urban areas data
+#'
+#' Urban area data from the US census
+#'
+#' @docType data
+#'
+#' @usage data(ua)
+#'
+#' @format An object of class \code{"sf", "data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(ua)
+"ua"
+
+#' Dialect regions: north/south/west
+#'
+#' Dialect region polygons for a rough division of the country into north, south, and west regions
+#'
+#' @docType data
+#'
+#' @usage data(nsw)
+#'
+#' @format An object of class \code{"sf", "data.frame"}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(nsw)
+"nsw"
+
+#' Dialect regions: ANAE
+#'
+#' Dialect region polygons for regions as defined in the Atlas of North American English.
+#'
+#' @docType data
+#'
+#' @usage data(anae)
+#'
+#' @format An object of class \code{"sf", "data.frame"}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(anae)
+"anae"
+
+#' Dialect regions: Carver
+#'
+#' Dialect region polygons following Carver (1987), American Regional Dialects.
+#'
+#' @docType data
+#'
+#' @usage data(carver)
+#'
+#' @format An object of class \code{"sf", "data.frame"}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(carver)
+"carver"
