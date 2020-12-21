76695725383dd5f0ddf334be385bcf0c6a7b5615
Kaija Gahm <kaija.gahm@gmail.com>
2020-12-21 10:00:26 -0500

    added forcats dependency

--- R/makeTables.R
+++ R/makeTables.R
@@ -223,6 +223,7 @@ makeConstructions <- function(constructions, con, updateID){
 #' @param con A connection to the existing database, created with RSQLite.
 #' @param updateID Character string that will become the `updateID` column for this table. For example, "survey11Add" for Survey 11.
 #' @param overwrite Whether or not to overwrite existing geocoded localities, if `geocoded` already exists in the environment. This parameter was mainly included for function testing and development--it pretty much never makes sense to set it to F when using the function.
+#' @import forcats
 #' @export
 
 # Make DEMO_GEO table -----------------------------------------------------
