4e1eb6018d3649a97e220ccb182cb19a1b371c0d
Kaija Gahm <kaija.gahm@gmail.com>
2020-12-30 10:24:08 -0500

    removed ua processing

--- R/processShapefiles.R
+++ R/processShapefiles.R
@@ -10,12 +10,12 @@ library(here)
 # Read in the shapefiles
 anae <- st_read(here("data-raw", "ANAE"))
 carver <- st_read(here("data-raw", "Carver"))
-ua <- st_read(here("data-raw", "censusUrbanAreas", "tl_2019_us_uac10"))
 nsw <- st_read(here("data-raw", "NSW"))
 counties <- st_read(here("data-raw", "USA_Counties", "v107", "counties.gdb"))
 
+# Processed UA separately, outside of the package in shrinkUAShapefile.R and saved it, compressed and with most columns removed, to the data/ folder of the package.
+
 use_data(anae, overwrite = TRUE, version = 3, compress = "xz")
 use_data(carver, overwrite = TRUE, version = 3, compress = "xz")
-use_data(ua, overwrite = TRUE, version = 3, compress = "xz")
 use_data(nsw, overwrite = TRUE, version = 3, compress = "xz")
 use_data(counties, overwrite = TRUE, version = 3, compress = "xz")
