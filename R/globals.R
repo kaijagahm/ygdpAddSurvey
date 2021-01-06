564b52e194d4a3a03c9ccf2dbffc18a8a8bd05cd
Kaija Gahm <kaija.gahm@gmail.com>
2021-01-06 13:16:20 -0500

    global variable definitions

---
+++ R/globals.R
@@ -0,0 +1,7 @@
+# Define as global variables the names of data columns
+utils::globalVariables(names(uaGeom))
+utils::globalVariables(names(uaCols))
+utils::globalVariables(names(nsw))
+utils::globalVariables(names(carver))
+utils::globalVariables(names(anae))
+utils::globalVariables(names(counties))
