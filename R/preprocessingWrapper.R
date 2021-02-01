060204eaddce296b9c4000630bd6036e49e12aa6
kaijagahm <kaija.gahm@gmail.com>
2021-02-01 16:15:10 -0500

    preprocessing doesn't require updateID

--- R/preprocessingWrapper.R
+++ R/preprocessingWrapper.R
@@ -5,7 +5,7 @@
 #' @param updateID Character string that will become the `updateID` column for this table. For example, "survey11Add" for Survey 11.
 #' @export
 
-preprocessData <- function(df, updateID){
+preprocessData <- function(df){
   df1 <- fixNames(df) # apply the fixNames function. See documentation for `fixNames()`
   df2 <- removeGeo(df1) # remove any extra geographic columns. See documentation for `removeGeo()`.
 
